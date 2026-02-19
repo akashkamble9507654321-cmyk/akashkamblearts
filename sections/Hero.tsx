161c8b6ca908d376e688232cc25580f6dbbec818
akashkamble9507654321-cmyk <akashkamble9507654321@gmail.com>
2026-02-19 04:13:52 -0930

    Update Hero.tsx

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -5,12 +5,7 @@ import { ArrowDownRight } from 'lucide-react';
 const Hero: React.FC = () => {
   return (
     <section
-     className="relative min-h-screen flex flex-col justify-center items-center px-6 md:px-12 text-center pt-20 overflow-hidden bg-cover bg-center bg-no-repeat"
-  style={{
-    backgroundImage: "url('/bgg.png')",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  }}
+    className="relative min-h-screen flex flex-col justify-center items-center px-6 md:px-12 text-center pt-20 overflow-hidden bg-cover bg-center" style={{ backgroundImage: "url('/bgg.png')" }}
 >
   {/* Subtle overlay for contrast */}
   <div className="absolute inset-0 bg-white/30"></div>
